5f773e91c49048aac53d5ad38a1c9dd789fb2390
NguyenMinhKiet <nguyenminhkiet642002@gmail.com>
2025-08-08 23:02:03 +0700

    feat: update showAble GlobalGoToTop button

--- src/components/GlobalStyle/index.tsx
+++ src/components/GlobalStyle/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, type ReactNode } from 'react';
+import { useEffect, useRef, useState, type ReactNode } from 'react';
 import './GlobalStyle.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +9,22 @@ interface GlobalStyleProps {
 }
 function GlobalStyle({ children }: GlobalStyleProps) {
     const globalTopRef = useRef<HTMLDivElement>(null);
+    const [showGoToTop, setShowGoToTop] = useState(false);
+
+    useEffect(() => {
+        const element = globalTopRef.current;
+        if (!element) return;
+
+        const observer = new ResizeObserver(() => {
+            const height = element.clientHeight;
+            const vh = window.visualViewport?.height || height;
+            setShowGoToTop(height >= vh);
+        });
+
+        observer.observe(element);
+
+        return () => observer.disconnect();
+    }, []);
 
     const handleScrollToTop = () => {
         globalTopRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -17,15 +33,17 @@ function GlobalStyle({ children }: GlobalStyleProps) {
     return (
         <div ref={globalTopRef} className="relative">
             {children}
-            <button
-                className="absolute bottom-5 right-3 border p-4 cursor-pointer rounded-full bg-[var(--main-color)] hover:bg-[var(--main-color-hover)] text-white"
-                onClick={handleScrollToTop}
-                data-tooltip-id="tooltip-globalGotoTop"
-                data-tooltip-content="Trở về đầu trang"
-            >
-                <FontAwesomeIcon icon={faArrowUp} />
-                <Tooltip id="tooltip-globalGotoTop" />
-            </button>
+            {showGoToTop && (
+                <button
+                    className="absolute bottom-5 right-3 border p-4 cursor-pointer rounded-full bg-[var(--main-color)] hover:bg-[var(--main-color-hover)] text-white"
+                    onClick={handleScrollToTop}
+                    data-tooltip-id="tooltip-globalGotoTop"
+                    data-tooltip-content="Trở về đầu trang"
+                >
+                    <FontAwesomeIcon icon={faArrowUp} />
+                    <Tooltip id="tooltip-globalGotoTop" />
+                </button>
+            )}
         </div>
     );
 }
