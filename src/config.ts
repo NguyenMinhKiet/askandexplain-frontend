95fbe5b4a4d1bd913a0ca571c4f66879e5b3d0e6
NguyenMinhKiet <nguyenminhkiet642002@gmail.com>
2025-08-17 22:19:11 +0700

    fix: add env domain

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME = import.meta.env.VITE_SITE_NAME || 'DefaultSite';
 export const BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT || '3000';
-export const DOMAIN_BACKEND = `http://localhost:${BACKEND_PORT}`;
+export const DOMAIN_BACKEND = import.meta.env.VITE_BACKEND_DOMAIN || `http://localhost:${BACKEND_PORT}`;
+// export const DOMAIN_BACKEND = `http://localhost:${BACKEND_PORT}`;
